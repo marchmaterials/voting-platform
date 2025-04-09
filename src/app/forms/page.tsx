fd8253f878696d210c8ca1c51e0ca265968047d8
Alex <alexservie2012@gmail.com>
2025-04-09 11:26:59 +0200

    more form inputs

--- src/app/forms/page.tsx
+++ src/app/forms/page.tsx
@@ -2,12 +2,83 @@ import Input from "../ui/Input";
 
 export default function Page() {
   return (
-    <div className="">
-      <h1>Submit a Project</h1>
-      <label id="title">Title</label>
-      <input type="text" id="title" />
+    <div className="flex flex-col gap-5 m-8 text-lg">
+      <h1 className="font-bold">Submit a Project</h1>
 
-      <Input type="text" id="description" label="description" />
+      <Input type="text" id="title" name="title" label="Title" />
+
+      <Input
+        type="text"
+        id="description"
+        name="description"
+        label="Description"
+      />
+
+      <Input
+        type="text"
+        id="location"
+        name="location"
+        label="Location of the completed project. (City, Country)"
+      />
+
+      <Input
+        type="number"
+        id="year_completed"
+        name="year_completed"
+        label="Year in which the project was completed"
+      />
+
+      <div>
+        <fieldset className="text-sm">
+          <legend className="text-lg">Typology:</legend>
+          <Input
+            type="radio"
+            id="residential"
+            name="typology"
+            label="Residential"
+          />
+          <Input
+            type="radio"
+            id="commercial"
+            name="typology"
+            label="Commercial"
+          />
+          <Input
+            type="radio"
+            id="industrial"
+            name="typology"
+            label="Industrial"
+          />
+          <Input
+            type="radio"
+            id="institutional"
+            name="typology"
+            label="Institutional / Infrastructure"
+          />
+        </fieldset>
+      </div>
+
+      <fieldset>
+        <h2>Materials Used</h2>
+        <Input
+          type="text"
+          id="material_name"
+          name="material_name"
+          label="Material Used"
+        />
+        <Input
+          type="text"
+          id="supplier_name"
+          name="supplier_name"
+          label="Supplier name"
+        />
+        <Input
+          type="text"
+          id="supplier_website"
+          name="supplier_website"
+          label="Supplier website"
+        />
+      </fieldset>
     </div>
   );
 }
--- src/app/ui/Input.tsx
+++ src/app/ui/Input.tsx
@@ -1,16 +1,17 @@
-const Input = ({ type, id, label }: InputProps) => {
-  return (
-    <>
-      <label id={id}>{label}</label>
-      <input type={type} id={id} className="h-10"></input>
-    </>
-  );
-};
-
-export default Input;
-
 type InputProps = {
   type: string;
   id: string;
   label: string;
+  name: string;
 };
+
+const Input = ({ type, id, label, name }: InputProps) => {
+  return (
+    <div className="flex justify-between align-center">
+      <label htmlFor={id}>{label}</label>
+      <input type={type} id={id} name={name} className="h-10"></input>
+    </div>
+  );
+};
+
+export default Input;
