8c40ad9147cc90ec92fac38ca8f733671853426c
Alex <alexservie2012@gmail.com>
2025-05-07 13:44:41 +0200

    cleanup

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,17 +1,7 @@
-import ws from "ws";
 import { PrismaClient } from "@prisma/client";
-import { PrismaNeon } from "@prisma/adapter-neon";
-import { Pool, PoolConfig, neonConfig } from "@neondatabase/serverless";
 
 const prismaClientSingleton = () => {
-  // neonConfig.webSocketConstructor = ws;
-  // const connectionString = `${process.env.DATABASE_URL}`;
-
-  // const pool = new Pool({ connectionString }) as unknown as PoolConfig;
-  // const adapter = new PrismaNeon(pool);
-  const prisma = new PrismaClient({ log: ["query", "info"] });
-
-  return prisma;
+  return new PrismaClient({ log: ["query", "info"] });
 };
 
 declare const globalThis: {
