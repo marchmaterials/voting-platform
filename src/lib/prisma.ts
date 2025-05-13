ae0b7d8b5c826e01bce9a0cfbf096fbbb0c3006a
Alex <alexservie2012@gmail.com>
2025-05-13 14:00:18 +0200

    add neon serverless driver again and use dev db in developement

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,21 @@
 import { PrismaClient } from "@prisma/client";
+import { neonConfig } from "@neondatabase/serverless";
+import ws from "ws";
+import { PrismaNeon } from "@prisma/adapter-neon";
+
+neonConfig.webSocketConstructor = ws;
 
 const prismaClientSingleton = () => {
-  return new PrismaClient({ log: ["query", "info"] });
+  const connectionString =
+    process.env.NODE_ENV === "production"
+      ? process.env.DATABASE_URL
+      : process.env.DATABASE_URL_DEVELOPMENT;
+  const adapter = new PrismaNeon({ connectionString });
+  return new PrismaClient({ adapter, log: ["query", "info"] });
 };
+// const prismaClientSingleton = () => {
+//   return new PrismaClient({ log: ["query", "info"] });
+// };
 
 declare const globalThis: {
   prismaGlobal: ReturnType<typeof prismaClientSingleton>;
