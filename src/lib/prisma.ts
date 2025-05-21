76f255a68de9882ef98051280b6a5957ab2a9966
Alex <alexservie2012@gmail.com>
2025-05-21 16:30:47 +0200

    update import path for prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../../prisma/generated/client";
 
 export const prismaClientSingleton = () => {
   console.log("PRISMACLIENTSINGLETON", process.env.DATABASE_URL);
