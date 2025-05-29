0de090295d059869282b7084480848a4db58d672
Alex <alexservie2012@gmail.com>
2025-05-29 13:06:11 +0200

    jotform embed

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -6,6 +6,7 @@ import type { UploadProps } from "antd";
 import { Button, message, Upload } from "antd";
 import { generateImagekitSignature } from "../actions/upload";
 import { upload } from "@imagekit/next";
+import Script from "next/script";
 
 const IMAGE_KIT_PUBLIC_KEY = "public_zippyGUFnPZ9M2RQ6pPgLqCwo4I=";
 
@@ -34,32 +35,28 @@ export default function Page() {
   //     setProjectInput(event.data);
   //   }
   // };
+  const handleReceiveMessage = (event) => {
+    console.log("listening", event);
+    if (event.origin !== "https://jotform.com") return; // optional security check
 
+    if (event.data.type === "input-value") {
+      console.log("Received input from iframe:", event.data.value);
+    }
+  };
   useEffect(() => {
     // window.addEventListener("message", handleReceiveMessage);
 
-    const testEmail = document.getElementById("input_2");
-    console.log("testEmail", testEmail);
-    console.log(
-      "testEmail value?",
-      (testEmail as HTMLInputElement | null)?.value
-    );
-    const jotformProjectTitle = document.querySelector("#input_49");
-    console.log("test project title:", jotformProjectTitle);
+    const iframe = document.getElementById(
+      "JotFormIFrame-251335007801345"
+    ) as HTMLIFrameElement | null; // or querySelector
 
-    const parentEmailInput = window.parent.document.getElementById("input_2");
-    const parentTitleInput = window.parent.document.getElementById("input_49");
-    const parentInput = window.parent.document.getElementById("input_2");
-    const parentQuery = window.parent.document.querySelector("#input_2");
-    console.log("parent window get by id??", parentInput);
-    console.log("parent query selector:", parentQuery);
-    if (parentEmailInput) {
-      setEmailInput(parentEmailInput as HTMLInputElement);
-    }
-    if (parentTitleInput) {
-      setTitleInput(parentTitleInput as HTMLInputElement);
-    }
-    // return () => window.removeEventListener("message", handleReceiveMessage);
+    // Ask the iframe to send input content
+    iframe?.contentWindow?.postMessage({ type: "get-input" }, "*");
+
+    // Listen for the response
+    window.addEventListener("message", handleReceiveMessage);
+
+    return () => window.removeEventListener("message", handleReceiveMessage);
   }, []);
   useEffect(() => {
     console.log("email input updated", emailInput);
@@ -103,89 +100,122 @@ export default function Page() {
   };
 
   return (
-    <form
-      onSubmit={async (e) => {
-        e.preventDefault();
-        if (!emailValue || !titleValue || files.length === 0) {
-          message.error(
-            "Missing required information. Please ensure other fields such as project title and email are complete."
-          );
-          return;
-        }
-        setLoading(true);
-        try {
-          const res = await Promise.all(
-            files.map(async (i): Promise<UploadResult | Error> => {
-              const { expire, token, signature } =
-                await generateImagekitSignature();
-              const uploadResponse = await upload({
-                file: i,
-                fileName: `march-competition.-.${titleValue}.-.${emailValue}.-.${i.name}`,
-                signature,
-                token,
-                expire,
-                publicKey: IMAGE_KIT_PUBLIC_KEY,
-                responseFields:
-                  "metadata, embeddedMetadata, customMetadata, tags",
-                extensions: [
-                  {
-                    name: "google-auto-tagging",
-                    maxTags: 20,
-                    minConfidence: 60,
-                  },
-                ],
-              });
-              if (!uploadResponse.fileId || !uploadResponse.url) {
-                throw new Error("Upload failed: missing fileId or url");
-              }
-              console.log("image upoload:", uploadResponse);
-              return {
-                fileId: uploadResponse.fileId,
-                url: uploadResponse.url,
-                AITags: uploadResponse.AITags,
-              } as UploadResult;
-            })
-          );
-          console.log("everything uploaded", res);
-          setSubmittedSuccess(true);
-          return res;
-        } catch (err) {
-          console.error("failed upload", err);
-        } finally {
-          setLoading(false);
-        }
-      }}
-    >
-      <div className="h-screen flex flex-col justify-center items-center">
-        {submittedSuccess && (
-          <h3>Thank you! Your images have successfully been submitted</h3>
-        )}
-        <div>
-          <Dragger {...props}>
-            <p className="ant-upload-drag-icon">
-              <InboxOutlined />
-            </p>
-            <p className="ant-upload-text">
-              Click or drag file to this area to upload
-            </p>
-            <p className="ant-upload-hint">
-              Please upload all of the images associated with this project
-              before clicking submit.
-            </p>
-          </Dragger>
-        </div>
-        <div>
-          <Button
-            className="m-5 p-4 mt-10"
-            type="primary"
-            loading={loading}
-            disabled={loading || submittedSuccess}
-            htmlType="submit"
-          >
-            Submit Images
-          </Button>
-        </div>
+    <>
+      <div className="flex flex-col justify-center items-center">
+        <iframe
+          id="JotFormIFrame-251335007801345"
+          title="The Sustainable Architecture Competition - Submission Form"
+          onload="window.parent.scrollTo(0,0)"
+          allowtransparency="true"
+          allow="geolocation; microphone; camera; fullscreen; payment"
+          src="https://form.jotform.com/251335007801345"
+          frameborder="0"
+          style={{
+            minWidth: "100%",
+            maxWidth: "100%",
+            height: "539px",
+            border: "none",
+          }}
+          scrolling="yes"
+        ></iframe>
+        <Script
+          src="https://cdn.jotfor.ms/s/umd/latest/for-form-embed-handler.js"
+          strategy="afterInteractive"
+          onLoad={() => {
+            // @ts-expect-error idk
+            if (window.jotformEmbedHandler) {
+              // @ts-expect-error idk
+              window.jotformEmbedHandler(
+                "iframe[id='JotFormIFrame-251335007801345']",
+                "https://form.jotform.com/"
+              );
+            }
+          }}
+        />
+
+        <form
+          onSubmit={async (e) => {
+            e.preventDefault();
+            if (!emailValue || !titleValue || files.length === 0) {
+              message.error(
+                "Missing required information. Please ensure other fields such as project title and email are complete."
+              );
+              return;
+            }
+            setLoading(true);
+            try {
+              const res = await Promise.all(
+                files.map(async (i): Promise<UploadResult | Error> => {
+                  const { expire, token, signature } =
+                    await generateImagekitSignature();
+                  const uploadResponse = await upload({
+                    file: i,
+                    fileName: `march-competition.-.${titleValue}.-.${emailValue}.-.${i.name}`,
+                    signature,
+                    token,
+                    expire,
+                    publicKey: IMAGE_KIT_PUBLIC_KEY,
+                    responseFields:
+                      "metadata, embeddedMetadata, customMetadata, tags",
+                    extensions: [
+                      {
+                        name: "google-auto-tagging",
+                        maxTags: 20,
+                        minConfidence: 60,
+                      },
+                    ],
+                  });
+                  if (!uploadResponse.fileId || !uploadResponse.url) {
+                    throw new Error("Upload failed: missing fileId or url");
+                  }
+                  console.log("image upoload:", uploadResponse);
+                  return {
+                    fileId: uploadResponse.fileId,
+                    url: uploadResponse.url,
+                    AITags: uploadResponse.AITags,
+                  } as UploadResult;
+                })
+              );
+              console.log("everything uploaded", res);
+              setSubmittedSuccess(true);
+              return res;
+            } catch (err) {
+              console.error("failed upload", err);
+            } finally {
+              setLoading(false);
+            }
+          }}
+        >
+          {submittedSuccess && (
+            <h3>Thank you! Your images have successfully been submitted</h3>
+          )}
+          <div className="mt-6">
+            <Dragger {...props}>
+              <p className="ant-upload-drag-icon">
+                <InboxOutlined />
+              </p>
+              <p className="ant-upload-text">
+                Click or drag file to this area to upload
+              </p>
+              <p className="ant-upload-hint">
+                Please upload all of the images associated with this project
+                before clicking submit.
+              </p>
+            </Dragger>
+          </div>
+          <div className="flex justify-center">
+            <Button
+              className="m-5 p-4 mt-10"
+              type="primary"
+              loading={loading}
+              disabled={loading || submittedSuccess}
+              htmlType="submit"
+            >
+              Submit Images
+            </Button>
+          </div>
+        </form>
       </div>
-    </form>
+    </>
   );
 }
