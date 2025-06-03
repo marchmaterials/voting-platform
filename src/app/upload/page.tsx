4e3f58b28208f4c1e9957b22f963c7ddced11444
Alex <alexservie2012@gmail.com>
2025-06-03 13:20:30 +0200

    add suspense and cleanup

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, Suspense } from "react";
 import { InboxOutlined } from "@ant-design/icons";
 import type { UploadProps } from "antd";
 import { Button, message, Upload } from "antd";
@@ -71,92 +71,94 @@ export default function Page() {
   };
 
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center">
-      <form
-        onSubmit={async (e) => {
-          e.preventDefault();
+    <Suspense>
+      <div className="min-h-screen flex flex-col justify-center items-center">
+        <form
+          onSubmit={async (e) => {
+            e.preventDefault();
 
-          if (!email || !projectTitle || files.length === 0) {
-            message.error(
-              "Missing required information, please upload at least one image."
-            );
-            return;
-          }
-          setLoading(true);
-          try {
-            const res = await Promise.all(
-              files.map(async (i): Promise<UploadResult | Error> => {
-                const { expire, token, signature } =
-                  await generateImagekitSignature();
-                const uploadResponse = await upload({
-                  file: i,
-                  fileName: `march-competition.-.${projectTitle}.-.${email}.-.${i.name}`,
-                  signature,
-                  token,
-                  expire,
-                  publicKey: IMAGE_KIT_PUBLIC_KEY,
-                  responseFields:
-                    "metadata, embeddedMetadata, customMetadata, tags",
-                  extensions: [
-                    {
-                      name: "google-auto-tagging",
-                      maxTags: 20,
-                      minConfidence: 60,
-                    },
-                  ],
-                });
-                if (!uploadResponse.fileId || !uploadResponse.url) {
-                  throw new Error("Upload failed: missing fileId or url");
-                }
-                console.log("image upoload:", uploadResponse);
-                return {
-                  fileId: uploadResponse.fileId,
-                  url: uploadResponse.url,
-                  AITags: uploadResponse.AITags,
-                } as UploadResult;
-              })
-            );
-            console.log("everything uploaded", res);
-            setSubmittedSuccess(true);
-            return res;
-          } catch (err) {
-            console.error("failed upload", err);
-          } finally {
-            setLoading(false);
-          }
-        }}
-      >
-        {submittedSuccess && (
-          <div className="flex justify-center text-green-600">
-            <h3>Thank you! Your images have successfully been submitted</h3>
+            if (!email || !projectTitle || files.length === 0) {
+              message.error(
+                "Missing required information, please upload at least one image."
+              );
+              return;
+            }
+            setLoading(true);
+            try {
+              const res = await Promise.all(
+                files.map(async (i): Promise<UploadResult | Error> => {
+                  const { expire, token, signature } =
+                    await generateImagekitSignature();
+                  const uploadResponse = await upload({
+                    file: i,
+                    fileName: `march-competition.-.${projectTitle}.-.${email}.-.${i.name}`,
+                    signature,
+                    token,
+                    expire,
+                    publicKey: IMAGE_KIT_PUBLIC_KEY,
+                    responseFields:
+                      "metadata, embeddedMetadata, customMetadata, tags",
+                    extensions: [
+                      {
+                        name: "google-auto-tagging",
+                        maxTags: 20,
+                        minConfidence: 60,
+                      },
+                    ],
+                  });
+                  if (!uploadResponse.fileId || !uploadResponse.url) {
+                    throw new Error("Upload failed: missing fileId or url");
+                  }
+                  console.log("image upoload:", uploadResponse);
+                  return {
+                    fileId: uploadResponse.fileId,
+                    url: uploadResponse.url,
+                    AITags: uploadResponse.AITags,
+                  } as UploadResult;
+                })
+              );
+              console.log("everything uploaded", res);
+              setSubmittedSuccess(true);
+              return res;
+            } catch (err) {
+              console.error("failed upload", err);
+            } finally {
+              setLoading(false);
+            }
+          }}
+        >
+          {submittedSuccess && (
+            <div className="flex justify-center text-green-600">
+              <h3>Thank you! Your images have successfully been submitted</h3>
+            </div>
+          )}
+          <div className="mt-6">
+            <Dragger {...props}>
+              <p className="ant-upload-drag-icon">
+                <InboxOutlined />
+              </p>
+              <p className="ant-upload-text">
+                Click or drag file to this area to upload
+              </p>
+              <p className="ant-upload-hint">
+                Please upload all of the images associated with this project
+                before clicking submit.
+              </p>
+            </Dragger>
           </div>
-        )}
-        <div className="mt-6">
-          <Dragger {...props}>
-            <p className="ant-upload-drag-icon">
-              <InboxOutlined />
-            </p>
-            <p className="ant-upload-text">
-              Click or drag file to this area to upload
-            </p>
-            <p className="ant-upload-hint">
-              Please upload all of the images associated with this project
-              before clicking submit.
-            </p>
-          </Dragger>
-        </div>
-        <div className="flex justify-center">
-          <Button
-            className="m-5 p-4 mt-10"
-            type="primary"
-            loading={loading}
-            disabled={loading || submittedSuccess}
-            htmlType="submit"
-          >
-            Submit Images
-          </Button>
-        </div>
-      </form>
-    </div>
+          <div className="flex justify-center">
+            <Button
+              className="m-5 p-4 mt-10"
+              type="primary"
+              loading={loading}
+              disabled={loading || submittedSuccess}
+              htmlType="submit"
+            >
+              Submit Images
+            </Button>
+          </div>
+        </form>
+      </div>
+    </Suspense>
   );
 }
