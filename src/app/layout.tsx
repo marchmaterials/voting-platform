b1bf33c8efd85d2cfc75ef9844688f41f890d07e
Alex <alexservie2012@gmail.com>
2025-06-05 13:38:24 +0200

    vercel analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { spaceGrotesk } from "@/app/ui/fonts";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -22,6 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${spaceGrotesk.className} antialiased`}>
         {children}
+        <Analytics/>
       </body>
     </html>
   );
