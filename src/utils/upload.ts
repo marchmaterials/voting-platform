6eeb3b287b3be5b024e163d0b6994ebaae6b5ef0
Alex <alexservie2012@gmail.com>
2025-07-01 13:35:43 +0200

    add dimensions check for image upload under 25mp

---
+++ src/utils/upload.ts
@@ -0,0 +1,24 @@
+"use client";
+
+export const checkImageDimensions = (file: File): Promise<boolean> => {
+  return new Promise((resolve) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.addEventListener("load", (event) => {
+      const _loadedImageUrl = event.target?.result;
+      const image = document.createElement("img");
+      image.src = _loadedImageUrl as string;
+
+      image.addEventListener("load", () => {
+        const { width, height } = image;
+
+        if (width * height > 25 * 1000 * 1000) {
+          console.log("failed dimension validation", width * height);
+          resolve(false);
+        }
+
+        resolve(true);
+      });
+    });
+  });
+};
