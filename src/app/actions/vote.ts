320b06732d52df545c751481e31277786b402a4c
Darlene Harsana <dharsana@ucsc.edu>
2025-07-29 15:58:23 +0200

    fix castVote function

--- src/app/actions/vote.ts
+++ src/app/actions/vote.ts
@@ -3,25 +3,20 @@
 import prisma from "@/lib/prisma";
 
 export async function castVote(projectID: string, email: string){
-    const user = await prisma.user.upsert({
-        where: { email },
-        create: { email },
-        update: {},
-    });
-
-    const [project, updatedUser]  = await prisma.$transaction([
+    const [project, user]  = await prisma.$transaction([
         prisma.project.update({
             where: { id: projectID },
             data: { votes: { increment: 1 } },
         }),
-        prisma.user.update({
-            where: { id: user.id },
-            data: { voteCount: { increment: 1 } },
+        prisma.user.upsert({
+            where: { email },
+            create: { email, voteCount: 1 },
+            update: { voteCount: { increment: 1 } },
         }),
     ]);
 
     return{
         projectVotes: project.votes,
-        userVotes: updatedUser.voteCount,
+        userVotes: user.voteCount,
     }
 }
