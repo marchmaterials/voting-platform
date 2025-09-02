7739e06b54fa648536ee5849e79903a74d454686
Alex <alexservie2012@gmail.com>
2025-09-02 20:48:13 +0200

    image migration script v1

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,5 @@
 export const IMAGE_KIT_PUBLIC_KEY = "public_zippyGUFnPZ9M2RQ6pPgLqCwo4I=";
 export const IMAGE_KIT_UPLOAD_URL =
   "https://upload.imagekit.io/api/v1/files/upload";
+export const IMAGE_KIT_BASE_URL = "https://ik.imagekit.io/march/";
 export const TOAST_NOTIFICATION_DURATION = 6;
