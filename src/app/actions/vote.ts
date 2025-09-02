4a79abd191d3ffb9617baf517e887ba3848b3b7b
Dean Shaff <dean.shaff@gmail.com>
2025-09-02 12:44:33 -0500

    change vote button position, make it so users have to wait 24 hours between votes

--- src/app/actions/vote.ts
+++ src/app/actions/vote.ts
@@ -2,21 +2,49 @@
 
 import prisma from "@/lib/prisma";
 
-export async function castVote(projectID: string, email: string){
-    const [project, user]  = await prisma.$transaction([
-        prisma.project.update({
-            where: { id: projectID },
-            data: { votes: { increment: 1 } },
-        }),
-        prisma.user.upsert({
-            where: { email },
-            create: { email, voteCount: 1 },
-            update: { voteCount: { increment: 1 } },
-        }),
-    ]);
+// this is one day, in millesconds
+const lastVoteInterval = 3600 * 1000 * 24
 
-    return{
-        projectVotes: project.votes,
-        userVotes: user.voteCount,
+export async function castVote(projectID: string, email: string) {
+    const [user] = await prisma.$transaction([prisma.user.findFirst({ where: { email } })]);
+    console.log(user)
+    if (user === null) {
+        const [project, user] = await prisma.$transaction([
+            prisma.project.update({
+                where: { id: projectID },
+                data: { votes: { increment: 1 } },
+            }),
+            prisma.user.create({
+                data: { voteCount: 1, email, lastVote: new Date() }
+            })
+        ]);
+        return {
+            projectVotes: project.votes,
+            userVotes: user.voteCount,
+        }
+    } else {
+        const now = new Date();
+        const diff = now - user.lastVote;
+        if (diff < lastVoteInterval) {
+            const waitingTime = lastVoteInterval - diff
+            console.log(waitingTime)
+            const msg = `You voted too recently! Wait another ${(waitingTime / (3600 * 1000)).toFixed(0)} hours to vote again!`
+            console.error(msg)
+            throw new Error(msg)
+        } else {
+            const [project, user] = await prisma.$transaction([
+                prisma.project.update({
+                    where: { id: projectID },
+                    data: { votes: { increment: 1 } },
+                }),
+                prisma.user.update({
+                    data: { voteCount: { increment: 1 }, email, lastVote: new Date() }
+                })
+            ]);
+            return {
+                projectVotes: project.votes,
+                userVotes: user.voteCount,
+            }
+        }
     }
 }
