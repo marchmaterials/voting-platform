7c478bada9229ba5480fe9babd34ecd627c00751
Dean Shaff <dean.shaff@gmail.com>
2025-09-06 12:40:04 -0500

    put in a little hack for the button -- not sure how to get it to play nice with antd

--- src/components/Lightbox.tsx
+++ src/components/Lightbox.tsx
@@ -32,7 +32,7 @@ export default function Lightbox({
       <div className="bg-white rounded-lg shadow-lg w-[90vw] h-[80vh] flex flex-col">
         <div className="flex flex-row items-center max-h-fit p-4">
           <h2 className="text-2xl font-extrabold pr-4">{title}</h2>
-          <VoteButton projectId={project.id} setVotes={setVotes} />
+          <VoteButton projectId={project.id} setVotes={setVotes} antdAdjustment={true} />
         </div>
         <div className="flex sm:flex-row flex-col-reverse flex-1 h-0 justify-around">
           <div className="h-full w-full sm:w-1/3 sm:max-w-[320px] overflow-y-auto">
