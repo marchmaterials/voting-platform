bf07f36914a5646c596fd3d997baedc067a084f0
Dean Shaff <dean.shaff@gmail.com>
2025-09-11 23:33:57 -0400

    see if reloading works

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,12 @@
 import ProjectDashboard from "@/components/ProjectDashboard";
 import { Suspense } from "react";
-// import { unstable_noStore as noStore } from 'next/cache';
+import { unstable_noStore as noStore } from 'next/cache';
 
-// export const revalidate = 0;            // disable ISR for everything under this layout
-// export const dynamic = "force-dynamic"; // force per-request SSR
+export const revalidate = 0;            // disable ISR for everything under this layout
+export const dynamic = "force-dynamic"; // force per-request SSR
 
 export default async function Page() {
-  // await noStore()
+  await noStore()
   return (
     <div className="min-h-screen">
       <div className="m-4">
