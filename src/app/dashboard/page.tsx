2b1ac0395398e459da182f46a24e0ac823008e4d
Dean Shaff <dean.shaff@gmail.com>
2025-09-11 23:30:09 -0400

    test whether changes work

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,12 @@
 import ProjectDashboard from "@/components/ProjectDashboard";
 import { Suspense } from "react";
+import { unstable_noStore as noStore } from 'next/cache';
 
-export default function Page() {
+// export const revalidate = 0;            // disable ISR for everything under this layout
+// export const dynamic = "force-dynamic"; // force per-request SSR
+
+export default async function Page() {
+  // await noStore()
   return (
     <div className="min-h-screen">
       <div className="m-4">
