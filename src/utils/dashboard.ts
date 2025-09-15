5bf794bc98df19a0ac4ad7f5e50f500679a61045
Dean Shaff <dean.shaff@gmail.com>
2025-09-15 12:54:13 -0400

    migrate to project stakeholder

--- src/utils/dashboard.ts
+++ src/utils/dashboard.ts
@@ -11,7 +11,7 @@ export const getAllProjects = async (): Promise<
       include: {
         images: true,
         projectMaterial: { include: { material: { include: { supplier: true } } } },
-        stakeholders: true,
+        projectStakeholders: { include: { stakeholder: true } },
       },
       orderBy: [{
         votes: "desc"
