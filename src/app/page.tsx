b70d9441d4b6b4e83b2d76f9740a31694935e3fa
Dean Shaff <dean.shaff@gmail.com>
2025-09-16 14:35:47 -0500

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import MiniDashboard from "@/components/MiniDashboard";
 import Image from "next/image";
 
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen pb-20 gap-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
