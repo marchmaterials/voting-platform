ad96179f1907ebac46d63b8e2af0e1ddf49f0221
Dean Shaff <dean.shaff@gmail.com>
2025-09-17 14:42:43 -0400

    add missing files

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -6,21 +6,9 @@ import {
   FullyEnrichedProject,
 } from "@/types/dashboard";
 import MaterialCollapse from "./MaterialCollapse";
-import { Collapse, Tag } from "antd";
-const comingSoonStakeholders = [
-  {
-    key: "1",
-    label: STAKEHOLDER_TYPE.ENGINEER,
-    children: <div className="text-sm font-light">Coming soon</div>,
-  },
-  {
-    key: "3",
-    label: STAKEHOLDER_TYPE.CONTRACTOR,
-    children: <div className="text-sm font-light">Coming soon</div>,
-  },
-];
+import StakeholderCollapse from "./StakeholderCollapse";
 
-type StakeholderRecord = Record<STAKEHOLDER_TYPE, Array<Stakeholder>>;
+
 
 export function Sidebar({
   materials,
@@ -30,43 +18,15 @@ export function Sidebar({
   project: FullyEnrichedProject;
 }) {
 
-  const materialList = materials.map(m => (<MaterialCollapse projectMaterial={m} />))
-  const stakeholdersMap: StakeholderRecord = project.projectStakeholders.reduce((acc, s) => {
-    const key = s.stakeholder.type[0]
-    if (!key) { return acc }
-    (acc[key] ??= []).push(s.stakeholder);
-    return acc
-  }, {} as Partial<StakeholderRecord>) as StakeholderRecord
-  const stakeholderList = Object.entries(stakeholdersMap)
-    .map(([type, sArray]) => {
-      const key = sArray.map(s => s.id).join(",")
-      const children = sArray.map(s => {
-        if (s.url !== null) {
-          return <div key={`${s.id}_`} className="text-sm font-light"><a href={s.url} rel="noopener noreferrer"
-            target="_blank"
-          >{s.companyName}</a></div>
-        } else {
-          <div key={`${s.id}_`} className="text-sm font-light">{s.companyName}</div>
-        }
-      })
-      return {
-        key: key,
-        label: type,
-        children: <div>{children}</div>
-      }
-    })
-    .concat(comingSoonStakeholders);
   return (
     <div className="pl-4 pr-4 pb-4">
       <h4 className="text-sm font-light">{project.location}</h4>
       <p className="text-sm font-light">{project.area}sqm</p>
       <p className="text-sm font-light">completed in {project.yearCompleted}</p>
       <h4 className="font-medium mt-4 mb-4">Materials</h4>
-      <div className="border-[1px] border-[#e5e7eb] rounded-lg overflow-hidden">
-        {materialList}
-      </div>
+      <MaterialCollapse projectMaterials={materials} />
       <h4 className="font-medium mt-4 mb-4">Stakeholders</h4>
-      <Collapse items={stakeholderList} />
+      <StakeholderCollapse projectStakeholders={project.projectStakeholders} />
       <p className="text-md mt-6">About this project:</p>
       <p className="text-sm font-light mt-2">{project.description}</p>
     </div>
