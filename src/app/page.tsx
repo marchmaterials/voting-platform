b86562164be4b2fb8aff9d97d223588e71a17194
Dean Shaff <dean.shaff@gmail.com>
2025-09-18 16:49:42 -0400

    add imprint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import Image from "next/image";
 
 export default function Home() {
@@ -47,60 +48,10 @@ export default function Home() {
             className="rounded-lg shadow-xl"
           ></video>
         </div>
-      </main>
-      <footer className="row-start-3 flex flex-row gap-6 items-center justify-between container">
-        <div className="flex flex-row">
-          <a
-            href="https://www.instagram.com/marchmaterials/"
-            target="blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/instagram.svg"
-              alt="instagram logo"
-              width={30}
-              height={30}
-              priority
-            />
-          </a>
-          <a
-            href="https://www.linkedin.com/company/marchmaterials/"
-            target="blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/linkedin.svg"
-              alt="linkedin logo"
-              width={30}
-              height={30}
-              priority
-            />
-          </a>
-        </div>
-        <div className="flex flex-row justify-around gap-2 h-10">
-          <Image
-            src="/avant-now.png"
-            alt="avant now accelerator fem track logo"
-            width={60}
-            height={20}
-            priority
-          />
-          <Image
-            src="https://www.berlin.de/i9f/r1/images/logo_berlin_m_srgb.svg"
-            alt="Berlin Senat Logo"
-            width={120}
-            height={20}
-            priority
-          />
-          <Image
-            src="/EUlogo.png"
-            alt="kofinanziert von der Europäischen Union"
-            width={180}
-            height={10}
-            priority
-          />
-        </div>
+      </main >
+      <footer className="row-start-3 gap-6 flex flex-row items-center justify-between container">
+        <Footer />
       </footer>
-    </div>
+    </div >
   );
 }
