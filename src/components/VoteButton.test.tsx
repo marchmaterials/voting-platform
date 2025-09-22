77b657a20989e2def76130804ec2bf67598a4180
Dean Shaff <dean.shaff@gmail.com>
2025-09-22 16:01:41 +0200

    start working on fixing tests

--- src/components/VoteButton.test.tsx
+++ src/components/VoteButton.test.tsx
@@ -19,42 +19,52 @@ describe("Votes Button", () => {
   });
 
   it("renders the vote button", async () => {
-    render(<VoteButton projectId="123" setVotes={mockSetVotes} />);
+    render(<VoteButton projectId="123" setVotes={mockSetVotes} antdAdjustment={false} />);
     expect(screen.getByRole("button", { name: /vote/i })).toBeInTheDocument();
   });
 
   it("opens the modal when the button is clicked", async () => {
-    render(<VoteButton projectId="123" setVotes={mockSetVotes} />);
+    render(<VoteButton projectId="123" setVotes={mockSetVotes} antdAdjustment={false} />);
     fireEvent.click(screen.getByRole("button", { name: /vote/i }));
     expect(await screen.findByTestId("email-input")).toBeInTheDocument();
   });
 
-  it("shows an error for invalid email", async () => {
-    render(<VoteButton projectId="123" setVotes={mockSetVotes} />);
+  it("shows an error if nothing is submitted", async () => {
+    render(<VoteButton projectId="123" setVotes={mockSetVotes} antdAdjustment={false} />);
     fireEvent.click(screen.getByRole("button", { name: /vote/i }));
     const submitButton = screen.getByRole("button", { name: /submit vote/i });
     fireEvent.click(submitButton);
 
     await waitFor(() => {
-      expect(screen.getByText(/invalid email address/i)).toBeInTheDocument();
+      expect(screen.getByText(/please enter your email/i)).toBeInTheDocument();
     });
 
     expect(castVote).not.toHaveBeenCalled();
   });
 
   it("submits a valid vote and updates vote count", async () => {
-    render(<VoteButton projectId="123" setVotes={mockSetVotes} />);
+    render(<VoteButton projectId="123" setVotes={mockSetVotes} antdAdjustment={false} />);
     fireEvent.click(screen.getByRole("button", { name: /vote/i }));
 
     const emailInput = screen.getByTestId("email-input");
     await userEvent.type(emailInput, "user@example.com");
 
+    // click the privacy policy
+    const privacyPolictInput = screen.getByTestId("privacy-policy-input");
+    await userEvent.click(privacyPolictInput);
+
+    // select an option from the profession drop down
+    const professionSelect = screen.getByRole("combobox");
+    await userEvent.click(professionSelect);
+    const professionOption = await screen.findByTitle("Architect");
+    await userEvent.click(professionOption)
+
     const submitButton = screen.getByRole("button", { name: /submit vote/i });
     fireEvent.click(submitButton);
 
     await waitFor(() => {
-      expect(castVote).toHaveBeenCalledWith("123", "user@example.com");
       expect(mockSetVotes).toHaveBeenCalledWith(5);
+      expect(castVote).toHaveBeenCalledWith("123", "user@example.com", "ARCHITECT", expect.any(String));
     });
   });
 });
