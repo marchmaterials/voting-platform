07884c39b9e6005aa0d48893bec97d1b15bc1361
Dean Shaff <dean.shaff@gmail.com>
2025-09-25 13:08:34 +0200

    fix bug in searchbar test

--- src/components/SearchBar.test.tsx
+++ src/components/SearchBar.test.tsx
@@ -1,14 +1,15 @@
-import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import SearchBar from "./SearchBar";
 import { useDataContext } from "@/app/context/dataContext";
-import { searchProjects } from "@/utils/dashboard";
+import { searchProjects } from "@/utils/search";
 import { generateProject } from "@/tests/generators";
+import { userEvent } from "@testing-library/user-event";
 
 // Mock dependencies
 jest.mock("@/app/context/dataContext", () => ({
-  useDataContext: jest.fn(),
+  useDataContext: jest.fn()
 }));
-jest.mock("@/utils/dashboard", () => ({
+jest.mock("@/utils/search", () => ({
   searchProjects: jest.fn(),
 }));
 
@@ -22,36 +23,36 @@ describe("SearchBar", () => {
     (useDataContext as jest.Mock).mockReturnValue({
       setFilteredProjects,
       setSearch,
+      allProjects: []
     });
   });
 
   it("sets search term and calls searchProjects", async () => {
-    (searchProjects as jest.Mock).mockResolvedValue([]);
+    (searchProjects as jest.Mock).mockReturnValue([]);
     render(<SearchBar setLoading={setLoading} />);
     const input = screen.getByTestId("search-bar");
     const button = screen.getByRole("button");
 
-    fireEvent.change(input, { target: { value: "Modern House" } });
-    fireEvent.click(button);
+    await userEvent.type(input, "Modern House");
+    await userEvent.click(button);
 
     await waitFor(() => {
       expect(setSearch).toHaveBeenCalledWith("Modern House");
-      expect(searchProjects).toHaveBeenCalledWith({
-        searchTerm: "Modern House",
-      });
+      expect(searchProjects).toHaveBeenCalledWith([], "Modern House");
     });
   });
 
   it("updates filtered projects on search", async () => {
     const project = generateProject();
-    (searchProjects as jest.Mock).mockResolvedValue([project]);
+
+    (searchProjects as jest.Mock).mockReturnValue([project]);
 
     render(<SearchBar setLoading={setLoading} />);
     const input = screen.getByTestId("search-bar");
     const button = screen.getByRole("button");
 
-    fireEvent.change(input, { target: { value: "Jane Doe" } });
-    fireEvent.click(button);
+    await userEvent.type(input, "Modern House");
+    await userEvent.click(button);
 
     await waitFor(() => {
       expect(setFilteredProjects).toHaveBeenCalledWith([project]);
@@ -63,9 +64,9 @@ describe("SearchBar", () => {
 
     render(<SearchBar setLoading={setLoading} />);
     const input = screen.getByTestId("search-bar");
-    fireEvent.change(input, { target: { value: searchTerm } });
+    await userEvent.type(input, searchTerm)
     const button = screen.getByRole("button");
-    fireEvent.click(button);
+    await userEvent.click(button);
 
     const inputAfterSearch = screen.getByTestId("search-bar");
 
