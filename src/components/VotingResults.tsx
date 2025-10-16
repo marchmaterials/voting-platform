6bee4a8b15e8afe840a5a197a53f964288d6e5be
Dean Shaff <dean.shaff@gmail.com>
2025-10-16 21:11:34 +0200

    add footer

--- src/components/VotingResults.tsx
+++ src/components/VotingResults.tsx
@@ -4,6 +4,7 @@ import Winner from "@/components/Winner";
 import { FullyEnrichedProject } from "@/types/dashboard";
 import Link from "next/link";
 import Masonry from "react-masonry-css";
+import VotingResultsFooter from "./VotingResultsFooter";
 
 
 function FloatingInfoBox({ title, subTitle }: { title: string, subTitle: string }) {
@@ -17,43 +18,46 @@ function FloatingInfoBox({ title, subTitle }: { title: string, subTitle: string 
 
 export default function VotingResults({ voteCount, projects }: { voteCount: number, projects: Array<FullyEnrichedProject> }) {
     const winners = projects.slice(0, 3)
-    const topTen = projects.slice(0, 11)
+    const topTen = projects.slice(3, 13)
 
     return (<>
-        <div
-            className="flex flex-col px-40 py-52 text-center justify-center items-center gap-4 mb-6 bg-cover bg-center"
-            style={{ backgroundImage: "url('/winner-hero.jpg')" }}
-        >
-            <p className="font-bold text-5xl text-white">{"Here's to the winners!"}</p>
-            <Link
-                href={`/dashboard`}
-                className="inline-flex items-center w-48 justify-center bg-march_green text-black px-4 py-2 rounded-full font-semibold hover:opacity-75 transition-colors"
+        <div className="bg-[#FBFEFA] pb-14">
+            <div
+                className="flex flex-col px-40 py-52 text-center justify-center items-center gap-4 mb-6 bg-cover bg-center"
+                style={{ backgroundImage: "url('/winner-hero.jpg')" }}
             >
-                Explore Projects
-            </Link>
+                <p className="font-bold text-5xl text-white">{"Here's to the winners!"}</p>
+                <Link
+                    href={`/dashboard`}
+                    className="inline-flex items-center w-48 justify-center bg-march_green text-black px-4 py-2 rounded-full font-semibold hover:opacity-75 transition-colors"
+                >
+                    Explore Projects
+                </Link>
+            </div>
+
+            <div className="flex flex-row flex-wrap gap-2 justify-evenly items-center max-w-[1000px] mx-auto px-10">
+                <FloatingInfoBox title={"100+"} subTitle={"Submissions"} />
+                <FloatingInfoBox title={"17"} subTitle={"Countries"} />
+                <FloatingInfoBox title={"36"} subTitle={"Finalists"} />
+                <FloatingInfoBox title={"3"} subTitle={"Winners"} />
+            </div>
+
+            <div className="flex flex-col items-center justify-center mt-28 text-center">
+                <h2 className="text-3xl font-bold mb-2">Community favorites</h2>
+                <p className="text-xl">
+                    Over <span className="font-bold">{voteCount} votes{" "}</span>were cast in just two months!
+                </p>
+                <p className="text-xl">Here are the top three projects chosen by the community</p>
+            </div>
         </div>
 
-        <div className="flex flex-row flex-wrap gap-2 justify-evenly items-center max-w-[1000px] mb-16 mx-auto px-10">
-            <FloatingInfoBox title={"100+"} subTitle={"Submissions"} />
-            <FloatingInfoBox title={"17"} subTitle={"Countries"} />
-            <FloatingInfoBox title={"36"} subTitle={"Finalists"} />
-            <FloatingInfoBox title={"3"} subTitle={"Winners"} />
-        </div>
-
-        <div className="flex flex-col items-center justify-center mb-16 text-center">
-            <h2 className="text-2xl font-bold mb-2">Community favorites</h2>
-            <p>
-                Over <span className="font-bold">{voteCount} votes{" "}</span>were cast in just two months!
-            </p>
-            <p>Here are the top three projects chosen by the community</p>
-        </div>
-
-        <div className="mb-10">
+        <div className="my-14">
             {winners.map((w, idx) => <Winner key={`w-${w.id}`} project={w} position={idx + 1} />)}
         </div>
 
-        <div className="flex flex-col text-center">
-            <h4 className="text-xl font-bold mb-10">Top 10</h4>
+        <div className="flex flex-col text-center mb-24 bg-[#FBFEFA] py-14">
+            <h4 className="text-xl font-bold">Top 10</h4>
+            <h4 className="text-l">Honarable Mentions</h4>
             <div className="flex items-center justify-center">
                 <Masonry
                     data-testid="project-grid"
@@ -63,8 +67,10 @@ export default function VotingResults({ voteCount, projects }: { voteCount: numb
                 >
                     {topTen.map(p => <ProjectCardNoVote key={p.id} project={p} />)}
                 </Masonry>
-
             </div>
         </div>
-    </>)
+        <VotingResultsFooter />
+        <div className="my-6"></div>
+    </>
+    )
 }
