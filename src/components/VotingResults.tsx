0db615171b9a1fe158498ca99f2d73257a67c791
Dean Shaff <dean.shaff@gmail.com>
2025-10-17 08:10:58 +0200

    build error fix

--- src/components/VotingResults.tsx
+++ src/components/VotingResults.tsx
@@ -7,14 +7,14 @@ import Masonry from "react-masonry-css";
 import VotingResultsFooter from "./VotingResultsFooter";
 
 
-function FloatingInfoBox({ title, subTitle }: { title: string, subTitle: string }) {
-    return (
-        <div className="min-w-[150px] flex-grow flex flex-col bg-[#277E25]/10 px-10 py-6 rounded-md text-center">
-            <h4 className="font-bold text-xl">{title}</h4>
-            <h5>{subTitle}</h5>
-        </div >
-    )
-}
+// function FloatingInfoBox({ title, subTitle }: { title: string, subTitle: string }) {
+//     return (
+//         <div className="min-w-[150px] flex-grow flex flex-col bg-[#277E25]/10 px-10 py-6 rounded-md text-center">
+//             <h4 className="font-bold text-xl">{title}</h4>
+//             <h5>{subTitle}</h5>
+//         </div >
+//     )
+// }
 
 export default function VotingResults({ voteCount, projects }: { voteCount: number, projects: Array<FullyEnrichedProject> }) {
     const winners = projects.slice(0, 3)
