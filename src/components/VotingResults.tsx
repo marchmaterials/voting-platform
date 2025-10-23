3a9993df47850ab0221a670d7acdf0b4882860c4
Dean Shaff <dean.shaff@gmail.com>
2025-10-23 21:47:34 +0200

    add judges

--- src/components/VotingResults.tsx
+++ src/components/VotingResults.tsx
@@ -5,6 +5,7 @@ import { FullyEnrichedProject } from "@/types/dashboard";
 import Link from "next/link";
 import Masonry from "react-masonry-css";
 import VotingResultsFooter from "./VotingResultsFooter";
+import Image from "next/image";
 
 type Category = "micro" | "small" | "medium" | "large";
 
@@ -42,11 +43,13 @@ function JudgeSelectionView({
   return (
     <>
       <div>
-        <div className="text-center font-bold text-3xl">{title}</div>
-        {selection.winners.map((w, idx) => (
-          <Winner key={`w-${w.id}`} project={w} position={idx + 1} />
-        ))}
-        <div className="text-center font-bold text-2xl">
+        <div className="text-center font-bold text-3xl mb-10">{title}</div>
+        <div className="flex flex-col sm:flex-row mx-auto items-center justify-center gap-8">
+          {selection.winners.map((w) => (
+            <Winner key={`w-${w.id}`} project={w} />
+          ))}
+        </div>
+        <div className="text-center font-bold text-2xl mb-10">
           {selection.honorableMentions.length == 1
             ? "Honourable Mention"
             : "Honourable Mentions"}
@@ -58,6 +61,133 @@ function JudgeSelectionView({
         </div>
       </div>
     </>
+  );
+}
+
+type JudgeFloatingHeadProps = {
+  name: string;
+  description: string;
+  src: string;
+};
+
+const judgeFloatingHeads: JudgeFloatingHeadProps[] = [
+  {
+    name: "Anna Heringer",
+    description: "Studio Anna Heringer",
+    src: "/anna_heringer.jpeg",
+  },
+  {
+    name: "Jan Jongert",
+    description: "Superuse Studios",
+    src: "/jan_jongert.jpeg",
+  },
+  {
+    name: "Olga Ioannou",
+    description: "TU Delft",
+    src: "/olga_ioannou.jpeg",
+  },
+  {
+    name: "Hattie Hartman",
+    description: "Architects' Journal",
+    src: "/hattie_hartman.jpeg",
+  },
+  {
+    name: "Noel Wibrand",
+    description: "Dorte Mandrup",
+    src: "/noel_wibrand.jpeg",
+  },
+  {
+    name: "Colin Chee",
+    description: "Never Too Small",
+    src: "/colin_chee.jpeg",
+  },
+  {
+    name: "Leila Behjat",
+    description: "Healthy Materials Lab",
+    src: "/leila_behjat.jpeg",
+  },
+  {
+    name: "Hilde Vanwildemeersch",
+    description: "Woonder",
+    src: "/hilde_vanwildemeersch.jpeg",
+  },
+  {
+    name: "Alexandra Georgescu",
+    description: "KOGAA",
+    src: "/alexandra_georgescu.jpeg",
+  },
+  {
+    name: "Ken De Cooman",
+    description: "BC Architects",
+    src: "/ken_de_cooman.jpeg",
+  },
+  {
+    name: "Werner Schönthaler",
+    description: "Hanfstein",
+    src: "/werner_schönthaler.jpeg",
+  },
+  {
+    name: "Imme Groet",
+    description: "Building Balance",
+    src: "/imme_groet.jpeg",
+  },
+  {
+    name: "Anna Pellizzari",
+    description: "Materially",
+    src: "/anna_pellizzari.jpeg",
+  },
+  {
+    name: "Christian Roggeman",
+    description: "Gramitherm",
+    src: "/christian_roggeman.jpeg",
+  },
+  {
+    name: "Juan Carlos Bamba",
+    description: "Bamba studio",
+    src: "/juan_carlos_bamba.jpeg",
+  },
+  {
+    name: "Dominik Campanella",
+    description: "Concular",
+    src: "/dominik_campanella.jpeg",
+  },
+  {
+    name: "Marie Roels",
+    description: "MARCH",
+    src: "/marie_roels.jpeg",
+  },
+];
+
+function JudgeFloatingHead({ name, description, src }: JudgeFloatingHeadProps) {
+  return (
+    <div className="flex flex-col text-center items-center w-32 mx-2 flex-shrink-0 mb-3">
+      <Image
+        src={src}
+        width={200}
+        height={200}
+        alt={`headshot of ${name}`}
+        className="w-28 h-28 object-cover rounded-full filter grayscale"
+      />
+      <p className={`font-bold text-s break-normal whitespace-normal mt-4`}>
+        {name}
+      </p>
+      <p className="text-xs">{description}</p>
+    </div>
+  );
+}
+
+function FloatingInfoBox({
+  title,
+  subTitle,
+}: {
+  title: string;
+  subTitle: string;
+}) {
+  return (
+    <div className="min-w-[150px] flex-grow flex flex-col bg-[#277E25]/10 px-10 py-6 rounded-md text-center">
+      <h4 className="font-bold text-xl">{title}</h4>
+      <h5>{subTitle}</h5>
+    </div>
   );
 }
 
@@ -106,12 +236,34 @@ export default function VotingResults({
             Get inspired
           </Link>
         </div>
+
+        <div className="flex flex-row flex-wrap gap-2 justify-evenly items-center max-w-[1000px] mx-auto px-10">
+          <FloatingInfoBox title={"100+"} subTitle={"Submissions"} />
+          <FloatingInfoBox title={"17"} subTitle={"Countries"} />
+          <FloatingInfoBox title={"36"} subTitle={"Finalists"} />
+          <FloatingInfoBox title={"3"} subTitle={"Winners"} />
+        </div>
+
+        <div className="flex flex-col text-center my-10 px-24">
+          <h2 className="text-2xl font-bold">Jury Awards</h2>
+          <p className="text-l">
+            <span className="font-bold">17 Judges</span> participated in the
+            competition, evaluating your projects and awarding their favourites
+          </p>
+        </div>
+        <div className="flex flex-wrap overflow-auto px-20 mx-auto justify-center">
+          {judgeFloatingHeads.map((p) => (
+            <JudgeFloatingHead key={p.name} {...p} />
+          ))}
+        </div>
+
         <div className="mt-14 sm:my-14">
           <JudgeSelectionView
             selection={judgeWinnersMap.large}
             title={"Large Category"}
           />
         </div>
+
         <div className="mt-14 sm:my-14">
           <JudgeSelectionView
             selection={judgeWinnersMap.medium}
